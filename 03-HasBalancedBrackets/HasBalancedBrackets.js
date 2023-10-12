36ae281300abdd9b851011704042f0af795c0df0
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-10-11 22:54:46 -0300

    resolved in another way

--- 03-HasBalancedBrackets/HasBalancedBrackets.js
+++ 03-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,17 +1,32 @@
+// function HasBalancedBrackets (string) {
+//   // Your code here:
+//   const stack = [];
+//   const validOpeningBrackets = ['[','{','('];
+//   const validClosingBrackets = [']','}',')'];
+
+//   for(let i = 0; i < string.length; i++){
+//     const bracket = string[i]
+
+//     if(validOpeningBrackets.includes(bracket)) stack.push(bracket)
+//     else if(validClosingBrackets.includes(bracket)){
+//       conversionToOpening = validOpeningBrackets[validClosingBrackets.indexOf(bracket)]
+//       if (!stack.length || stack.pop() !== conversionToOpening) return false;
+//     }
+//   }
+//   return !stack.length
+// }
+
 function HasBalancedBrackets (string) {
-  // Your code here:
   const stack = [];
-  const validOpeningBrackets = ['[','{','('];
-  const validClosingBrackets = [']','}',')'];
+  const validBrackets = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  }
 
-  for(let i = 0; i < string.length; i++){
-    const bracket = string[i]
-
-    if(validOpeningBrackets.includes(bracket)) stack.push(bracket)
-    else if(validClosingBrackets.includes(bracket)){
-      conversionToOpening = validOpeningBrackets[validClosingBrackets.indexOf(bracket)]
-      if (!stack.length || stack.pop() !== conversionToOpening) return false;
-    }
+  for(const bracket of string){
+    if(validBrackets[bracket]) stack.push(bracket)
+    else if(validBrackets[stack.pop()] !== bracket) return false;
   }
   return !stack.length
 }
