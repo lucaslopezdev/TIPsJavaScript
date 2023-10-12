ac19c932c33f6cb08248724e5dc30cb6c986e781
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-10-12 00:12:39 -0300

    FindNeedle solved in a more beautiful way

--- 01-FindNeedle/FindNeedle.js
+++ 01-FindNeedle/FindNeedle.js
@@ -1,14 +1,22 @@
-function FindNeedle (haystack, needle) {
-  // Your code here:
-  for(let i = 0; i < haystack.length - needle.length; i++){
-    let found = true
-    for(let j = 0; j < needle.length; j++){
-      if(haystack[i+j] !== needle[j]) {
-        found = false;
-        break;
-      };
-    }
-    if(found) return i;
+// function FindNeedle (haystack, needle) {
+//   // Your code here:
+//   for(let i = 0; i < haystack.length - needle.length; i++){
+//     let found = true
+//     for(let j = 0; j < needle.length; j++){
+//       if(haystack[i+j] !== needle[j]) {
+//         found = false;
+//         break;
+//       };
+//     }
+//     if(found) return i;
+//   }
+//   return -1;
+// }
+
+function FindNeedle(haystack, needle) {
+
+  for (let i = 0; i < haystack.length; i++) {
+     if (haystack.slice(i, i + needle.length) === needle) return i;
   }
   return -1;
 }
