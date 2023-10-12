dc01e42a3bfa8219335d1576076f9c06b8837143
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-10-12 00:11:24 -0300

    FindNeedle solved

--- 01-FindNeedle/FindNeedle.js
+++ 01-FindNeedle/FindNeedle.js
@@ -1,6 +1,16 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
-
+  for(let i = 0; i < haystack.length - needle.length; i++){
+    let found = true
+    for(let j = 0; j < needle.length; j++){
+      if(haystack[i+j] !== needle[j]) {
+        found = false;
+        break;
+      };
+    }
+    if(found) return i;
+  }
+  return -1;
 }
 
 module.exports = FindNeedle
