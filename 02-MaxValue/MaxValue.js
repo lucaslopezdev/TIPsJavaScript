5f7561e88e511e5a8683d9092715f60ea7403043
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-10-13 10:31:53 -0300

    MaxValue solved with 0(n2)

--- 02-MaxValue/MaxValue.js
+++ 02-MaxValue/MaxValue.js
@@ -1,6 +1,13 @@
 function MaxValue (shares) {
   // Your code here:
-
+  let maximo = -Infinity;
+  for(let i = 0; i < shares.length - 1; i++){
+    for(let j = i + 1; j < shares.length; j++){
+      let newMax = shares[j] - shares[i]
+      if( newMax > maximo) maximo = newMax 
+    }
+  }
+  return maximo
 }
 
 module.exports = MaxValue
