094392369aa1719e1b5dc7e92422e9e649dce5e4
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-10-13 11:20:00 -0300

    Solved with 0(n)

--- 02-MaxValue/MaxValue.js
+++ 02-MaxValue/MaxValue.js
@@ -1,13 +1,25 @@
+// function MaxValue (shares) {
+//   // Your code here:
+//   let max = -Infinity;
+//   for(let i = 0; i < shares.length - 1; i++){
+//     for(let j = i + 1; j < shares.length; j++){
+//       let newMax = shares[j] - shares[i]
+//       if( newMax > max) max = newMax 
+//     }
+//   }
+//   return max
+// }
+
 function MaxValue (shares) {
-  // Your code here:
-  let maximo = -Infinity;
-  for(let i = 0; i < shares.length - 1; i++){
-    for(let j = i + 1; j < shares.length; j++){
-      let newMax = shares[j] - shares[i]
-      if( newMax > maximo) maximo = newMax 
-    }
+  let minPrice = shares[0], maxProfit = -Infinity; 
+  // Or maxProfit = 0 if I want to know only how much I won and not if I lost
+
+  for(let i = 1; i < shares.length; i++){
+    if(shares[i] < minPrice) minPrice = shares[i];
+    if(shares[i] - minPrice > maxProfit) maxProfit = shares[i] - minPrice;
   }
-  return maximo
+
+  return maxProfit;
 }
 
 module.exports = MaxValue
